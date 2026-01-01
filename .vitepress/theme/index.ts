b8eb0d6a867fc807ebf8a5719870242e461628d4
Simon Pangan <simonjoseph.pangan@gmail.com>
2026-01-02 00:37:56 +0800

    add sass as a dev dependency and rename style.css to style.scss

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,7 +2,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
-import './style.css'
+import './style.scss'
 
 export default {
   extends: DefaultTheme,
