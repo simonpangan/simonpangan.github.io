6af6613cf349b1d1408fa844582faf62591b9b55
Simon Pangan <simonjoseph.pangan@gmail.com>
2026-01-04 14:37:48 +0800

    custom vp hero update

--- .vitepress/theme/components/CustomVPHero.vue
+++ .vitepress/theme/components/CustomVPHero.vue
@@ -3,22 +3,24 @@ import { VPImage } from 'vitepress/theme'
 </script>
 
 <template>
-  <div class="VPHero has-image">
+  <div class="VPHero">
     <div class="container">
-      <div class="main">
-        <slot name="home-hero-info-before" />
-        <slot name="home-hero-info">
-          <h1 class="heading">
-            <span class="name clip">Simon Pangan</span>
-            <span class="text">adsasd</span>
-          </h1>
-          <p class="tagline">adsasd</p>
-        </slot>
-      </div>
       <div class="image">
         <div class="image-container">
-          <VPImage class="image-src" image="/assets/img/avatar.jpg" />
+<!--          <img src="/assets/img/avatar.jpg">-->
+<!--          asda-->
+<!--          <VPImage-->
+<!--              class="image-src"-->
+<!--              image="/assets/img/avatar.jpg"-->
+<!--          />-->
         </div>
+      </div>
+      <div class="main">
+        <h1 class="name">
+          <span class="clip">Simon Pangan</span>
+        </h1>
+        <p class="description">Developing engaging and performant web apps with Laravel & PHP</p>
+        <p class="location">Manila, Philippines</p>
       </div>
     </div>
   </div>
@@ -32,65 +34,65 @@ import { VPImage } from 'vitepress/theme'
 
 @media (min-width: 640px) {
   .VPHero {
+    background: blue;
     padding: calc(var(--vp-nav-height) + var(--vp-layout-top-height, 0px) + 80px) 48px 64px;
   }
 }
 
 @media (min-width: 960px) {
   .VPHero {
+    background: red;
     padding: calc(var(--vp-nav-height) + var(--vp-layout-top-height, 0px) + 80px) 64px 64px;
   }
 }
 
 .container {
+  background: pink;
   display: flex;
   flex-direction: column;
   margin: 0 auto;
   max-width: 1152px;
 }
 
-@media (min-width: 960px) {
-  .container {
-    flex-direction: row;
-  }
-}
-
 .main {
-  position: relative;
-  z-index: 10;
-  order: 2;
-  flex-grow: 1;
-  flex-shrink: 0;
-}
-
-.VPHero.has-image .container {
   text-align: center;
-}
-
-@media (min-width: 960px) {
-  .VPHero.has-image .container {
-    text-align: left;
-  }
+  //position: relative;
+  //z-index: 10;
+  //order: 2;
+  //flex-grow: 1;
+  //flex-shrink: 0;
 }
 
 @media (min-width: 960px) {
   .main {
-    order: 1;
-    width: calc((100% / 3) * 2);
-  }
-
-  .VPHero.has-image .main {
-    max-width: 592px;
+    //order: 1;
+    //width: calc((100% / 3) * 2);
+    //background: orange;
+    //max-width: 592px;
   }
 }
 
-.heading {
-  display: flex;
-  flex-direction: column;
+.image {
+  //order: 1;
+  margin: -76px -24px -48px;
 }
 
-.name,
-.text {
+@media (min-width: 640px) {
+  .image {
+    margin: -108px -24px -48px;
+  }
+}
+
+@media (min-width: 960px) {
+  .image {
+    //flex-grow: 1;
+    //order: 2;
+    margin: 0;
+    min-height: 100%;
+  }
+}
+
+.name {
   width: fit-content;
   max-width: 392px;
   letter-spacing: -0.4px;
@@ -100,9 +102,19 @@ import { VPImage } from 'vitepress/theme'
   white-space: pre-wrap;
 }
 
-.VPHero.has-image .name,
-.VPHero.has-image .text {
-  margin: 0 auto;
+@media (min-width: 640px) {
+  .name {
+    max-width: 576px;
+    line-height: 56px;
+    font-size: 48px;
+  }
+}
+
+@media (min-width: 960px) {
+  .name {
+    line-height: 64px;
+    font-size: 56px;
+  }
 }
 
 .name {
@@ -116,29 +128,7 @@ import { VPImage } from 'vitepress/theme'
   -webkit-text-fill-color: var(--vp-home-hero-name-color);
 }
 
-@media (min-width: 640px) {
-  .name,
-  .text {
-    max-width: 576px;
-    line-height: 56px;
-    font-size: 48px;
-  }
-}
-
-@media (min-width: 960px) {
-  .name,
-  .text {
-    line-height: 64px;
-    font-size: 56px;
-  }
-
-  .VPHero.has-image .name,
-  .VPHero.has-image .text {
-    margin: 0;
-  }
-}
-
-.tagline {
+.location {
   padding-top: 8px;
   max-width: 392px;
   line-height: 28px;
@@ -148,12 +138,12 @@ import { VPImage } from 'vitepress/theme'
   color: var(--vp-c-text-2);
 }
 
-.VPHero.has-image .tagline {
+.VPHero.has-image .location {
   margin: 0 auto;
 }
 
 @media (min-width: 640px) {
-  .tagline {
+  .location {
     padding-top: 12px;
     max-width: 576px;
     line-height: 32px;
@@ -162,87 +152,69 @@ import { VPImage } from 'vitepress/theme'
 }
 
 @media (min-width: 960px) {
-  .tagline {
+  .location {
     line-height: 36px;
     font-size: 24px;
   }
 
-  .VPHero.has-image .tagline {
+  .VPHero.has-image .location {
     margin: 0;
-  }
-}
-
-.image {
-  order: 1;
-  margin: -76px -24px -48px;
-}
-
-@media (min-width: 640px) {
-  .image {
-    margin: -108px -24px -48px;
-  }
-}
-
-@media (min-width: 960px) {
-  .image {
-    flex-grow: 1;
-    order: 2;
-    margin: 0;
-    min-height: 100%;
-  }
-}
-
-.image-container {
-  position: relative;
-  margin: 0 auto;
-  width: 320px;
-  height: 320px;
-}
-
-@media (min-width: 640px) {
-  .image-container {
-    width: 392px;
-    height: 392px;
-  }
-}
-
-@media (min-width: 960px) {
-  .image-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 100%;
-    /*rtl:ignore*/
-    transform: translate(-32px, -32px);
-  }
-}
-
-:deep(.image-src) {
-  position: absolute;
-  top: 50%;
-  /*rtl:ignore*/
-  left: 50%;
-  max-width: 192px;
-  max-height: 192px;
-  width: 100%;
-  height: 100%;
-  object-fit: contain;
-  /*rtl:ignore*/
-  transform: translate(-50%, -50%);
-}
-
-@media (min-width: 640px) {
-  :deep(.image-src) {
-    max-width: 256px;
-    max-height: 256px;
-  }
-}
-
-@media (min-width: 960px) {
-  :deep(.image-src) {
-    max-width: 320px;
-    max-height: 320px;
   }
 }
 </style>
+
+<!--<style scoped>-->
+<!--.image-container {-->
+<!--  position: relative;-->
+<!--  margin: 0 auto;-->
+<!--  width: 320px;-->
+<!--  height: 320px;-->
+<!--}-->
+
+<!--@media (min-width: 640px) {-->
+<!--  .image-container {-->
+<!--    width: 392px;-->
+<!--    height: 392px;-->
+<!--  }-->
+<!--}-->
+
+<!--@media (min-width: 960px) {-->
+<!--  .image-container {-->
+<!--    display: flex;-->
+<!--    justify-content: center;-->
+<!--    align-items: center;-->
+<!--    //width: 100%;-->
+<!--    //height: 100%;-->
+<!--    /*rtl:ignore*/-->
+<!--    transform: translate(-32px, -32px);-->
+<!--  }-->
+<!--}-->
+
+<!--:deep(.image-src) {-->
+<!--  position: absolute;-->
+<!--  top: 50%;-->
+<!--  /*rtl:ignore*/-->
+<!--  left: 50%;-->
+<!--  max-width: 192px;-->
+<!--  max-height: 192px;-->
+<!--  width: 100%;-->
+<!--  height: 100%;-->
+<!--  object-fit: contain;-->
+<!--  /*rtl:ignore*/-->
+<!--  transform: translate(-50%, -50%);-->
+<!--}-->
+
+<!--@media (min-width: 640px) {-->
+<!--  :deep(.image-src) {-->
+<!--    max-width: 256px;-->
+<!--    max-height: 256px;-->
+<!--  }-->
+<!--}-->
+
+<!--@media (min-width: 960px) {-->
+<!--  :deep(.image-src) {-->
+<!--    max-width: 320px;-->
+<!--    max-height: 320px;-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
