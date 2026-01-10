f9b730cbf34a1bd1aa92fd438f46404015532b9b
Simon Pangan <simonjoseph.pangan@gmail.com>
2026-01-11 01:00:05 +0800

    add custom Layout component with BlogsList integration

---
+++ .vitepress/theme/components/Layout.vue
@@ -0,0 +1,14 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import BlogsList from "./BlogsList.vue";
+
+const { Layout } = DefaultTheme
+</script>
+
+<template>
+  <Layout>
+    <template #home-hero-after>
+      <BlogsList />
+    </template>
+  </Layout>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,15 +2,12 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme-without-fonts'
+import Layout from './components/Layout.vue'
 import './styles/main.scss'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
+  Layout: Layout,
   enhanceApp({ app, router, siteData }) {
     // ...
   }
