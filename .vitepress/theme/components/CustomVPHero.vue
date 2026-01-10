f1102ba277948af7e5541decd311e004a9fd85e4
Simon Pangan <simonjoseph.pangan@gmail.com>
2026-01-11 00:20:55 +0800

    update VPHero

--- .vitepress/theme/components/CustomVPHero.vue
+++ .vitepress/theme/components/CustomVPHero.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="VPHero">
-    <div class="container mx-auto">
-      <div class="main text-center">
-        <h1 class="name clip">Simon Pangan</h1>
-        <div class="description">Developing engaging and performant web apps with Laravel & PHP</div>
-        <div class="location">Manila, Philippines</div>
+    <div class="container">
+      <div class="text-center">
+        <h1 class="title">
+          Hi, I'm Simon Pangan! 👋
+        </h1>
+        <div class="mt-1">
+          A Full Stack Developer based in Philippines
+        </div>
       </div>
     </div>
   </div>
@@ -32,62 +35,13 @@
   display: flex;
   flex-direction: column;
   max-width: 1152px;
+  margin: 0 auto;
 }
 
-.name {
+.title {
   letter-spacing: -0.4px;
   line-height: 40px;
-  font-size: 32px;
+  font-size: 2.2em;
   font-weight: 700;
-  white-space: pre-wrap;
-}
-
-@media (min-width: 640px) {
-  .name {
-    line-height: 56px;
-    font-size: 48px;
-  }
-}
-
-@media (min-width: 960px) {
-  .name {
-    line-height: 64px;
-    font-size: 56px;
-  }
-}
-
-.name {
-  color: var(--vp-home-hero-name-color);
-}
-
-.clip {
-  background: var(--vp-home-hero-name-background);
-  -webkit-background-clip: text;
-  background-clip: text;
-  -webkit-text-fill-color: var(--vp-home-hero-name-color);
-}
-
-.location {
-  padding-top: 8px;
-  line-height: 28px;
-  font-size: 18px;
-  font-weight: 500;
-  white-space: pre-wrap;
-  //color: var(--vp-c-text-2);
-}
-
-@media (min-width: 640px) {
-  .location {
-    padding-top: 12px;
-    line-height: 32px;
-    font-size: 20px;
-  }
-}
-
-@media (min-width: 960px) {
-  .location {
-    line-height: 36px;
-    font-size: 24px;
-  }
 }
 </style>
