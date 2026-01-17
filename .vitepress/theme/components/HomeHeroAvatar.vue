6da5f04851b85b693dc1b18785fd35b9ae440de3
Simon Pangan <simonjoseph.pangan@gmail.com>
2026-01-18 00:48:51 +0800

    initial HomeHeroAvatar.vue

---
+++ .vitepress/theme/components/HomeHeroAvatar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="content">
+    <img src="/assets/img/avatar.png" width="135" height="135" class="avatar" />
+  </div>
+</template>
+
+<style scoped>
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.avatar {
+  border-radius: 50%;
+  border: 5px solid var(--vp-avatar-border);
+}
+
+@keyframes avatar-transform {
+  from {
+    transform: rotate(0);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.avatar:hover {
+  animation: 5s linear 0s infinite avatar-transform;
+}
+</style>
